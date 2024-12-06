9b416e25c980159cd2fa808fdd45671bff7ea385
Egert Linnas <egert.linnas@voco.ee>
2024-12-06 15:32:43 +0200

    Refactor code duplicate with promise for data fetching

--- index.js
+++ index.js
@@ -11,44 +11,40 @@ app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstra
 const key = '80e689237748f150f57c360028cbbe21' // API key
 let city = 'Tartu'
 
-app.get('/', (req, res) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    .then((response) => {
-        return response.json()
+const getWeatherData = (city) => {
+    return new Promise((resolve, reject) => {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}` 
+        fetch(url)
+        .then((response) => {
+            return response.json()
         })
         .then((data) => {
             let description = data.weather[0].description
             let city = data.name
-            let temp = Math.round(parseFloat(data.main.temp)-273.15)
-            console.log(description)
-            console.log(city)
-            console.log(temp)
-            res.render('index', {
+            let temp = Math.round(parseFloat(data.main.temp)-273.15) 
+            const result = {
                 description: description,
                 city: city,
                 temp: temp
-            })
+            } 
+            resolve(result)
         })
-})
-
-app.post('/', (req, res) => {
-   let city = req.body.cityname
-   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-   .then((response) => {
-    return response.json()
+        .catch(error => {
+            reject(error)
+        })
     })
+} 
+
+app.all('/', (req, res) => {
+    let city
+    if(req.method == 'GET'){
+        city = 'Tartu'
+    } else if (req.method == 'POST'){
+        city = req.body.cityname
+    }
+    getWeatherData(city)
     .then((data) => {
-        let description = data.weather[0].description
-        let city = data.name
-        let temp = Math.round(parseFloat(data.main.temp)-273.15)
-        console.log(description)
-        console.log(city)
-        console.log(temp)
-        res.render('index', {
-            description: description,
-            city: city,
-            temp: temp
-        })
+        res.render('index', data)
     })
 })
 
